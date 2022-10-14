5ea6c2961278410d22bc1a8939173f05c1bc8159
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-10-13 23:18:50 -0300

    intro in the loops and some relational operators.

---
+++ loops/aula_10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int count = 0;
+    int number = 1;
+
+    for (count; count <= 10; count++){
+        printf("---\n");
+        printf("tests %d\n", count);
+    }
+
+    return 0;
+}
